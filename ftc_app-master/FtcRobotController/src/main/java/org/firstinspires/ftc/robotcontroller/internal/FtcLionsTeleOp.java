e508b7e00f0132c0d22cf46efa070d3d7e118c23
Libby <eleanor_mather@horacemann.org>
2016-11-28 15:50:39 -0500

    Ms. Screen's Computer's Version

--- ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcLionsTeleOp.java
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcLionsTeleOp.java
@@ -1,41 +1,14 @@
-/* Copyright (c) 2014 Qualcomm Technologies Inc
 
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Qualcomm Technologies Inc nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
-
-package com.qualcomm.ftcrobotcontroller.opmodes;
+//package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import android.bluetooth.BluetoothClass;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import android.app.Activity;
@@ -50,12 +23,15 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
-
+@TeleOp(name="TeleOp", group="TeleOp")
 public class FTCLionsTeleOp extends OpMode {
+
     final boolean DEBUG = true;
 
-    DcMotor leftDrive;
-    DcMotor rightDrive;
+    DcMotor leftBack;
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor rightBack;
 
     public FTCLionsTeleOp() {
 
@@ -63,17 +39,17 @@ public class FTCLionsTeleOp extends OpMode {
 
     @Override
     public void start() {
-        started = true;
+        boolean  started = true;
 
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
 
-        leftFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        rightFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        leftBack.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        rightBack.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
@@ -86,9 +62,9 @@ public class FTCLionsTeleOp extends OpMode {
     public void loop() {
         if (DEBUG) {
             // TELEMETRY FOR JOYSTICK DEBUGGING
-            telemetry.addData("****Controller Data****");
-            telemetry.addData("Gamepad1 Movement: y", gamepad1.right_stick_y, ", x", gamepad1.right_stick_x);
-            telemetry.addData("Gamepad2 Movement: y", gamepad2.right_stick_y, ", x", gamepad2.right_stick_x);
+//            telemetry.addData(gamepad1.left_stick_y);
+//            telemetry.addData("Gamepad1 Movement: y", gamepad1.right_stick_y, ", x", gamepad1.right_stick_x);
+//            telemetry.addData("Gamepad2 Movement: y", gamepad2.right_stick_y, ", x", gamepad2.right_stick_x);
         }
 
         ////////////////////////////////
@@ -97,21 +73,24 @@ public class FTCLionsTeleOp extends OpMode {
 
 
         // TANK DRIVE \\
-        leftFront.setPower(gamepad1.left_stick_y);
-        rightFront.setPower(-gamepad1.right_stick_y);
-        leftBack.setPower(gamepad1.left_stick_y);
-        rightBack.setPower(-gamepad1.right_stick_y);
+        leftFront.setPower(-gamepad1.left_stick_y / 1.4); // /1.4 for general power issues considering the robot is somewhat tipsy
+        rightFront.setPower(gamepad1.right_stick_y / 1.4);
+        leftBack.setPower(-gamepad1.left_stick_y / 1.4);
+        rightBack.setPower(gamepad1.right_stick_y / 1.4);
 
-        while (gamepad1.right_stick_x != 0) { //right side of robot
-            rightFront.setPower(gamepad1.right_stick_x); //assuming that the right side is backwards
-            rightBack.setPower(-gamepad1.right_stick_x);
+        while (gamepad1.right_trigger != 0) { //right side of robot
+            rightFront.setPower(-gamepad1.right_trigger / (2 - .7)); //assuming that the right side is backwards
+            rightBack.setPower(gamepad1.right_trigger / (2 - .7));
+            leftFront.setPower(gamepad1.right_trigger / (2 + 0));
+            leftBack.setPower(-gamepad1.right_trigger / (2 + 0));
 
         }
-        while (gamepad1.left_stick_x != 0) { //left side of the robot
-            leftFront.setPower(gamepad1.left_stick_x);
-            leftBack.setPower(-gamepad1.left_stick_x);
+        while (gamepad1.left_trigger != 0) { //left side of the robot
+            rightFront.setPower(gamepad1.left_trigger / (2 + 0)); //assuming that the right side is backwards
+            rightBack.setPower(-gamepad1.left_trigger / (2 + 0));
+            leftFront.setPower(-gamepad1.left_trigger / (2 - .7));
+            leftBack.setPower(gamepad1.left_trigger / (2 - .7)); //.7 for the general power per side and .8 for the wheels moving forward
         }
-
 
 
 
@@ -123,18 +102,19 @@ public class FTCLionsTeleOp extends OpMode {
 
 
 
-        
-
         // E-STOP
         if (gamepad1.left_bumper && gamepad1.right_bumper && gamepad2.left_bumper && gamepad2.right_bumper) { //mash those bumpers
-            leftDrive.setPower(0);
-            rightDrive.setPower(0);
+            leftFront.setPower(0);
+            leftBack.setPower(0);
+            rightFront.setPower(0);
+            rightBack.setPower(0);
         }
     }
-}
+
 
     @Override
     public void stop() {
 
     }
 }
+
